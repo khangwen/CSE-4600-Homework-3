574caed1b43737bb01c7268be93de8916ed77f48
Alan Nguyen <a_wen197@yahoo.com>
2022-11-13 14:21:00 -0800

    rename main->singlethread and made multithread.cpp

--- singlethread.cpp
+++ singlethread.cpp
@@ -1,5 +1,3 @@
-// task 1
-
 #include <iostream>
 
 using namespace std;
@@ -65,57 +63,3 @@ int main()
 
     return 0;
 }
-
-// task 2
-/*
-#include <iostream>
-
-using namespace std;
-
-string csu(int *size)
-{
-    return "helo world\n";
-}
-
-string sb(int *size)
-{
-    return "helo world\n";
-}
-
-string csusb(int *size)
-{
-    return "helo world\n";
-}
-
-string number(int *size)
-{
-    return "helo world\n";
-}
-
-int main()
-{
-    int size;
-
-    try
-    {
-        cout << "Please input a number from 1 - 50 for the array size\n";
-        cin >> size;
-        if (size < 1 || size > 50)
-            throw "Number out of bound\n";
-    }
-    catch (const char *msg)
-    {
-        cerr << msg << endl;
-        return 0;
-    }
-
-    int *array[size];
-
-    for (int i = 0; i < sizeof(array); i++){
-
-    }
-
-
-    return 0;
-}
-*/
