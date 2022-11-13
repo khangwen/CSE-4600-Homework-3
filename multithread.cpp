f52a5f6a975c59a526b2e8f1178f877e635fedab
Alan Nguyen <a_wen197@yahoo.com>
2022-11-13 14:45:02 -0800

    copy pasted from singlethread.cpp again

--- multithread.cpp
+++ multithread.cpp
@@ -2,62 +2,62 @@
 
 using namespace std;
 
-void *csu()
+void *csu() // csu helper function
 {
     cout << "csu  ";
 }
 
-void *sb()
+void *sb() // sb helper function
 {
     cout << "sb  ";
 }
 
-void *csusb()
+void *csusb() // csusb helper function
 {
     cout << "csusb  ";
 }
 
-void *number(int size)
+void *number(int size) // number helper function
 {
     cout << size << "  ";
 }
 
-int main()
+int main() // main function
 {
-    int size;
+    int size; // size of array -> to be inputted
 
-    try
+    try // throw exception if out of bound
     {
         cout << "Please input a number from 1 - 50 for the array size\n";
-        cin >> size;
+        cin >> size; // user inputs size
         if (size < 1 || size > 50)
             throw "Number out of bound\n";
     }
     catch (const char *msg)
     {
-        cerr << msg << endl;
-        return 0;
+        cerr << msg << endl; // returns exception message
+        return 0;            // exits program
     }
 
-    int array[size];
-    int mod1, mod2;
+    int array[size]; // array initialized with inputted size
+    int mod1, mod2;  // modulus 1 and 2
 
     for (int i = 0; i < size; i++)
     {
-        array[i] = i + 1;
+        array[i] = i + 1; // assign array elements with numbers
     }
 
     for (int i = 0; i < size; i++)
     {
         mod1 = array[i] % 2;
         mod2 = array[i] % 5;
-        if (mod1 == 0 && mod2 == 0)
+        if (mod1 == 0 && mod2 == 0) // checks if divisible by 2 AND 5
             csusb();
-        else if (mod1 != 0 && mod2 != 0)
+        else if (mod1 != 0 && mod2 != 0) // neither divisible by 2 or 5
             number(array[i]);
-        else if (mod1 == 0 && mod2 != 0)
+        else if (mod1 == 0 && mod2 != 0) // divisible by 2 but not 5
             csu();
-        else if (mod2 == 0 && mod1 != 0)
+        else if (mod2 == 0 && mod1 != 0) // divisible by 5 but not 2
             sb();
     }
 
